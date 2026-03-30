38437e87062b266778e2d056226fc7d09a1883aa
SofiaEzechiello <sofiaezechiello@gmail.com>
2026-03-30 16:14:09 -0300

    Add temperature feedback based on user input

---
+++ Aula-4/Ex04-temperatura/programs/temperatura.py
@@ -0,0 +1,7 @@
+temp = float(input("Digite a temperatura do ambiente (°C): "))
+if temp <= 18:
+    print("Está frio.")
+elif temp <= 28:
+    print("Está agradável.")
+else:
+    print("Está quente.")
